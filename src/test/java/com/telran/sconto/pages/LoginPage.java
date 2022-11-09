5d22126b00bbe4cde450e5a9c61abbdbba8b9973
Natalia1903 <98782051+Natalia1903@users.noreply.github.com>
2022-11-09 18:00:25 +0100

    CucumberSelenide_Maven added

---
+++ src/test/java/com/telran/sconto/pages/LoginPage.java
@@ -0,0 +1,37 @@
+package com.telran.sconto.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import com.telran.sconto.utils.PropertiesLoader;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+
+    private static By loginIcon = By.cssSelector(".headerElement--login");
+    private static By loginHeader = By.cssSelector(".existingAccount__headline");
+    private static By emailInput = By.id("loginEmail");
+    private static By passwordInput = By.id("loginPassword");
+    private static String validEmail = PropertiesLoader.loadProperty("valid.email");
+    private static String validPassword = PropertiesLoader.loadProperty("valid.password");
+
+    public void clickOnLoginIcon() {
+        $(loginIcon).click();
+    }
+
+    private static By submitButton = By.id("login-submit");
+
+
+    public void validLoginInput() {
+        $(emailInput).setValue(validEmail);
+        $(passwordInput).setValue(validPassword);
+    }
+    public SelenideElement loginPageHeaderDisplayed() {
+        return $(loginHeader);
+
+}
+
+    public void clickOnLoginSubmitButton() {
+        $(submitButton).click();
+    }
+}
